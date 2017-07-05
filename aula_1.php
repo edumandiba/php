fc10213ae1e4c14326b6b1a02a6e34f815a6a51d
Eduardo Gomes <edumandiba@gmail.com>
2017-07-05 11:34:19 -0300

    aula_1.php

--- aula_1.php
+++ aula_1.php
@@ -1,9 +1,24 @@
 <?php
-echo "Meu <strong>primeiro- modificado para teste</strong> programa";
-echo "Acrescentei esta linha";
-echo "muito legal o git";
+echo "Meu <strong>primeiro- modificado para teste</strong> programa". "<br>";
+echo "Acrescentei esta linha". "<br>";
+echo "muito legal o git". "<br>";
 
-echo "Criando um conflito";
+echo "Criando um conflito" . "<br>";
 
+// escopo de variáveis , coloco a palavra global para que a variaval $nome possa ser usada fora do escopo da funcao nome.
+// na funcao nome 2 , ou eu utilizo a palavra global antes de $nome ou defino um nome por exemplo $nome = "carlos".
+$nome = "Eduardo";
+function nome(){
 
+global $nome;
+echo $nome;
+}
+
+function nome2(){
+   global $nome;
+    echo $nome;
+}
+nome($nome);
+echo "<br>";
+    nome2 ($nome);
 ?>
